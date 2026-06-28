13322d561fb72e13d07bed26efa0e56e0d6abed2
Kevin Liu <kevinjqliu@users.noreply.github.com>
2026-06-28 18:13:11 -0400

    refactor: group TPC-DS table modules (#291)

--- tpcdsgen-arrow/src/lib.rs
+++ tpcdsgen-arrow/src/lib.rs
@@ -15,32 +15,8 @@
 //! assert_eq!(batch.num_columns(), 3);
 //! ```
 
-mod call_center;
-mod catalog_page;
-mod catalog_returns;
-mod catalog_sales;
 pub mod conversions;
-mod customer;
-mod customer_address;
-mod customer_demographics;
-mod date_dim;
-mod dbgen_version;
-mod household_demographics;
-mod income_band;
-mod inventory;
-mod item;
-mod promotion;
-mod reason;
-mod ship_mode;
-mod store;
-mod store_returns;
-mod store_sales;
-mod time_dim;
-mod warehouse;
-mod web_page;
-mod web_returns;
-mod web_sales;
-mod web_site;
+mod tables;
 
 use std::collections::VecDeque;
 
@@ -49,31 +25,13 @@ use arrow::datatypes::SchemaRef;
 use tpcdsgen::config::Session;
 use tpcdsgen::row::{GeneratedRow, RowGenerator};
 
-pub use call_center::CallCenterArrow;
-pub use catalog_page::CatalogPageArrow;
-pub use catalog_returns::CatalogReturnsArrow;
-pub use catalog_sales::CatalogSalesArrow;
-pub use customer::CustomerArrow;
-pub use customer_address::CustomerAddressArrow;
-pub use customer_demographics::CustomerDemographicsArrow;
-pub use date_dim::DateDimArrow;
-pub use dbgen_version::DbgenVersionArrow;
-pub use household_demographics::HouseholdDemographicsArrow;
-pub use income_band::IncomeBandArrow;
-pub use inventory::InventoryArrow;
-pub use item::ItemArrow;
-pub use promotion::PromotionArrow;
-pub use reason::ReasonArrow;
-pub use ship_mode::ShipModeArrow;
-pub use store::StoreArrow;
-pub use store_returns::StoreReturnsArrow;
-pub use store_sales::StoreSalesArrow;
-pub use time_dim::TimeDimArrow;
-pub use warehouse::WarehouseArrow;
-pub use web_page::WebPageArrow;
-pub use web_returns::WebReturnsArrow;
-pub use web_sales::WebSalesArrow;
-pub use web_site::WebSiteArrow;
+pub use tables::{
+    CallCenterArrow, CatalogPageArrow, CatalogReturnsArrow, CatalogSalesArrow,
+    CustomerAddressArrow, CustomerArrow, CustomerDemographicsArrow, DateDimArrow,
+    DbgenVersionArrow, HouseholdDemographicsArrow, IncomeBandArrow, InventoryArrow, ItemArrow,
+    PromotionArrow, ReasonArrow, ShipModeArrow, StoreArrow, StoreReturnsArrow, StoreSalesArrow,
+    TimeDimArrow, WarehouseArrow, WebPageArrow, WebReturnsArrow, WebSalesArrow, WebSiteArrow,
+};
 
 /// An iterator of Arrow [`RecordBatch`]es that also exposes its schema.
 pub trait RecordBatchIterator: Iterator<Item = RecordBatch> + Send {
--- tpcdsgen/src/row/mod.rs
+++ tpcdsgen/src/row/mod.rs
@@ -1,109 +1,56 @@
 pub mod abstract_row_generator;
-pub mod call_center_row;
-pub mod call_center_row_generator;
-pub mod catalog_page_row;
-pub mod catalog_page_row_generator;
-pub mod catalog_returns_row;
-pub mod catalog_returns_row_generator;
-pub mod catalog_sales_row;
-pub mod catalog_sales_row_generator;
-pub mod customer_address_row;
-pub mod customer_address_row_generator;
-pub mod customer_demographics_row;
-pub mod customer_demographics_row_generator;
-pub mod customer_row;
-pub mod customer_row_generator;
-pub mod date_dim_row;
-pub mod date_dim_row_generator;
-pub mod dbgen_version_row;
-pub mod dbgen_version_row_generator;
 pub mod generated_row;
-pub mod household_demographics_row;
-pub mod household_demographics_row_generator;
-pub mod income_band_row;
-pub mod income_band_row_generator;
-pub mod inventory_row;
-pub mod inventory_row_generator;
-pub mod item_row;
-pub mod item_row_generator;
-pub mod promotion_row;
-pub mod promotion_row_generator;
-pub mod reason_row;
-pub mod reason_row_generator;
 pub mod row_generator;
-pub mod ship_mode_row;
-pub mod ship_mode_row_generator;
-pub mod store_returns_row;
-pub mod store_returns_row_generator;
-pub mod store_row;
-pub mod store_row_generator;
-pub mod store_sales_row;
-pub mod store_sales_row_generator;
 pub mod table_row;
-pub mod time_dim_row;
-pub mod time_dim_row_generator;
-pub mod warehouse_row;
-pub mod warehouse_row_generator;
-pub mod web_page_row;
-pub mod web_page_row_generator;
-pub mod web_returns_row;
-pub mod web_returns_row_generator;
-pub mod web_sales_row;
-pub mod web_sales_row_generator;
-pub mod web_site_row;
-pub mod web_site_row_generator;
+mod tables;
 
 pub use abstract_row_generator::AbstractRowGenerator;
-pub use call_center_row::CallCenterRow;
-pub use call_center_row_generator::CallCenterRowGenerator;
-pub use catalog_page_row::CatalogPageRow;
-pub use catalog_page_row_generator::CatalogPageRowGenerator;
-pub use catalog_returns_row::CatalogReturnsRow;
-pub use catalog_returns_row_generator::CatalogReturnsRowGenerator;
-pub use catalog_sales_row::CatalogSalesRow;
-pub use catalog_sales_row_generator::CatalogSalesRowGenerator;
-pub use customer_address_row::CustomerAddressRow;
-pub use customer_address_row_generator::CustomerAddressRowGenerator;
-pub use customer_demographics_row::CustomerDemographicsRow;
-pub use customer_demographics_row_generator::CustomerDemographicsRowGenerator;
-pub use customer_row::CustomerRow;
-pub use customer_row_generator::CustomerRowGenerator;
-pub use date_dim_row::DateDimRow;
-pub use date_dim_row_generator::DateDimRowGenerator;
-pub use dbgen_version_row::DbgenVersionRow;
-pub use dbgen_version_row_generator::DbgenVersionRowGenerator;
 pub use generated_row::GeneratedRow;
-pub use household_demographics_row::HouseholdDemographicsRow;
-pub use household_demographics_row_generator::HouseholdDemographicsRowGenerator;
-pub use income_band_row::IncomeBandRow;
-pub use income_band_row_generator::IncomeBandRowGenerator;
-pub use inventory_row::InventoryRow;
-pub use inventory_row_generator::InventoryRowGenerator;
-pub use item_row::ItemRow;
-pub use item_row_generator::ItemRowGenerator;
-pub use promotion_row::PromotionRow;
-pub use promotion_row_generator::PromotionRowGenerator;
-pub use reason_row::ReasonRow;
-pub use reason_row_generator::ReasonRowGenerator;
 pub use row_generator::{RowGenerator, RowGeneratorResult};
-pub use ship_mode_row::ShipModeRow;
-pub use ship_mode_row_generator::ShipModeRowGenerator;
-pub use store_returns_row::StoreReturnsRow;
-pub use store_returns_row_generator::StoreReturnsRowGenerator;
-pub use store_row::StoreRow;
-pub use store_row_generator::StoreRowGenerator;
-pub use store_sales_row::StoreSalesRow;
-pub use store_sales_row_generator::StoreSalesRowGenerator;
 pub use table_row::TableRow;
-pub use time_dim_row::TimeDimRow;
-pub use time_dim_row_generator::TimeDimRowGenerator;
-pub use warehouse_row::WarehouseRow;
-pub use warehouse_row_generator::WarehouseRowGenerator;
-pub use web_page_row::WebPageRow;
-pub use web_page_row_generator::WebPageRowGenerator;
-pub use web_returns_row::WebReturnsRow;
-pub use web_returns_row_generator::WebReturnsRowGenerator;
-pub use web_sales_row::WebSalesRow;
-pub use web_sales_row_generator::WebSalesRowGenerator;
-pub use web_site_row::WebSiteRow;
-pub use web_site_row_generator::WebSiteRowGenerator;
+pub use tables::{
+    call_center_row, call_center_row_generator, catalog_page_row, catalog_page_row_generator,
+    catalog_returns_row, catalog_returns_row_generator, catalog_sales_row,
+    catalog_sales_row_generator, customer_address_row, customer_address_row_generator,
+    customer_demographics_row, customer_demographics_row_generator, customer_row,
+    customer_row_generator, date_dim_row, date_dim_row_generator, dbgen_version_row,
+    dbgen_version_row_generator, household_demographics_row, household_demographics_row_generator,
+    income_band_row, income_band_row_generator, inventory_row, inventory_row_generator, item_row,
+    item_row_generator, promotion_row, promotion_row_generator, reason_row, reason_row_generator,
+    ship_mode_row, ship_mode_row_generator, store_returns_row, store_returns_row_generator,
+    store_row, store_row_generator, store_sales_row, store_sales_row_generator, time_dim_row,
+    time_dim_row_generator, warehouse_row, warehouse_row_generator, web_page_row,
+    web_page_row_generator, web_returns_row, web_returns_row_generator, web_sales_row,
+    web_sales_row_generator, web_site_row, web_site_row_generator,
+};
+pub use tables::{
+    call_center_row::CallCenterRow, call_center_row_generator::CallCenterRowGenerator,
+    catalog_page_row::CatalogPageRow, catalog_page_row_generator::CatalogPageRowGenerator,
+    catalog_returns_row::CatalogReturnsRow,
+    catalog_returns_row_generator::CatalogReturnsRowGenerator, catalog_sales_row::CatalogSalesRow,
+    catalog_sales_row_generator::CatalogSalesRowGenerator,
+    customer_address_row::CustomerAddressRow,
+    customer_address_row_generator::CustomerAddressRowGenerator,
+    customer_demographics_row::CustomerDemographicsRow,
+    customer_demographics_row_generator::CustomerDemographicsRowGenerator,
+    customer_row::CustomerRow, customer_row_generator::CustomerRowGenerator,
+    date_dim_row::DateDimRow, date_dim_row_generator::DateDimRowGenerator,
+    dbgen_version_row::DbgenVersionRow, dbgen_version_row_generator::DbgenVersionRowGenerator,
+    household_demographics_row::HouseholdDemographicsRow,
+    household_demographics_row_generator::HouseholdDemographicsRowGenerator,
+    income_band_row::IncomeBandRow, income_band_row_generator::IncomeBandRowGenerator,
+    inventory_row::InventoryRow, inventory_row_generator::InventoryRowGenerator, item_row::ItemRow,
+    item_row_generator::ItemRowGenerator, promotion_row::PromotionRow,
+    promotion_row_generator::PromotionRowGenerator, reason_row::ReasonRow,
+    reason_row_generator::ReasonRowGenerator, ship_mode_row::ShipModeRow,
+    ship_mode_row_generator::ShipModeRowGenerator, store_returns_row::StoreReturnsRow,
+    store_returns_row_generator::StoreReturnsRowGenerator, store_row::StoreRow,
+    store_row_generator::StoreRowGenerator, store_sales_row::StoreSalesRow,
+    store_sales_row_generator::StoreSalesRowGenerator, time_dim_row::TimeDimRow,
+    time_dim_row_generator::TimeDimRowGenerator, warehouse_row::WarehouseRow,
+    warehouse_row_generator::WarehouseRowGenerator, web_page_row::WebPageRow,
+    web_page_row_generator::WebPageRowGenerator, web_returns_row::WebReturnsRow,
+    web_returns_row_generator::WebReturnsRowGenerator, web_sales_row::WebSalesRow,
+    web_sales_row_generator::WebSalesRowGenerator, web_site_row::WebSiteRow,
+    web_site_row_generator::WebSiteRowGenerator,
+};
